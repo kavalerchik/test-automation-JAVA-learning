9969a21e1b1c1f3b3824ed242846db87f95515a1
kava <yair@carambo.la>
2015-12-30 19:05:31 +0200

    upload the java file

---
+++ NavigateCommands.java
@@ -0,0 +1,30 @@
+package automationFramework;
+	
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class NavigateCommands {
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driverff = new FirefoxDriver();
+		
+		String URL = "http://www.DemoQA.com";
+		driverff.get(URL);
+
+		driverff.findElement(By.xpath("/html/body/div/div/div[2]/aside[1]/div[2]/div/ul/li/a")).click();
+		
+		driverff.navigate().back();
+		
+		driverff.navigate().forward();
+		
+		driverff.navigate().to(URL);
+		
+		driverff.navigate().refresh();
+		
+		//driver.close();
+	}
+
+}
